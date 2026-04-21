df4741bc36397a60a31075a232acb0a749173c81
Kavindu_T_Jayarathne <110294883+kavin53@users.noreply.github.com>
2026-04-21 21:55:39 +0530

    Implemented VirtualQueryEx scanning loop

---
+++ version3.py
@@ -0,0 +1,170 @@
+from ctypes import *
+from ctypes.wintypes import *
+
+kernel32 = windll.kernel32
+
+DEBUG_PROCESS = 0x00000001
+CREATE_NEW_CONSOLE = 0x00000010
+
+DBG_CONTINUE = 0x00010002
+DBG_EXCEPTION_NOT_HANDLED = 0x80010001
+
+EXCEPTION_DEBUG_EVENT =1
+CREATE_PROCESS_DEBUG_EVENT = 3
+EXIT_PROCESS_DEBUG_EVENT = 5
+
+EXCEPTION_BREAKPOINT = 0x80000003
+
+class STARTUPINFO(Structure):
+    _fields_ = [
+        ("cb", DWORD),
+        ("lpReserved", LPSTR),
+        ("lpDesktop", LPSTR),
+        ("lpTitle", LPSTR),
+        ("dwX", DWORD),
+        ("dwY", DWORD),
+        ("dwXSize", DWORD),
+        ("dwYSize", DWORD),
+        ("dwXCountChars", DWORD),
+        ("dwYCountChars", DWORD),
+        ("dwFillAttribute", DWORD),
+        ("dwFlags", DWORD),
+        ("wShowWindow", WORD),
+        ("cbReserved2", WORD),
+        ("lpReserved2", LPBYTE),
+        ("hStdInput", HANDLE),
+        ("hStdOutput", HANDLE),
+        ("hStdError", HANDLE),
+    ]
+
+class PROCESSINFO(Structure):
+    _fields_ = [
+        ("hProcess", HANDLE),
+        ("hThread" , HANDLE),
+        ("dwProcessId", DWORD),
+        ("dwThreadId", DWORD),
+    ]
+
+class DEBUG_EVENT(Structure):
+    _fields_ = [
+        ("dwDebugEventCode", DWORD),
+        ("dwProcessId", DWORD),
+        ("dwThreadId", DWORD),
+        ("u" , c_byte*160)
+    ]
+
+class Debugger:
+    def __init__(self):
+        self.hProcess = None
+        self.pid = None
+        self.breakpoints = {}
+
+    def load(self,path):
+        si = STARTUPINFO()
+        si.cb = sizeof(si)
+
+        pi = PROCESSINFO()
+
+        success = kernel32.CreateProcessA(
+            path.encode(),
+            None,
+            None,
+            None,
+            False,
+            DEBUG_PROCESS | CREATE_NEW_CONSOLE ,
+            None,
+            None,
+            byref(si),
+            byref(pi),
+        )
+
+        if not success:
+            print("Launch Failed")
+            return False
+
+        self.hProcess = pi.hProcess
+        self.pid = pi.dwProcessId
+
+        print(f"Process ID: {self.pid}")
+        return True
+
+    def write_memory(self, address , data):
+
+        count = c_size_t(0)
+
+        kernel32.WriteProcessMemory(
+            self.hProcess,
+            address,
+            data,
+            len(data),
+            byref(count)
+        )
+
+    def read_momory(self , address , size):
+        buffer = create_string_buffer(size)
+        count = c_size_t(0)
+
+        kernel32.ReadProcessMemory(
+            self.hProcess,
+            address,
+            buffer,
+            size,
+            byref(count)
+        )
+
+        return buffer.raw
+
+    def set_breakpoint(self,address):
+        original_byte = self.read_memory(address,1)
+        self.breakpoints[address] = original_byte
+
+        self.write_memory(address, b'\xCC')
+
+        print(f"[+] Breakpoint Set at {hex(address)}")
+
+
+    def run(self):
+        debug_event = DEBUG_EVENT()
+
+        while True:
+            if kernel32.WaitForDebugEvent(byref(debug_event),1000):
+
+                code = debug_event.dwDebugEventCode
+
+                if code == CREATE_PROCESS_DEBUG_EVENT:
+                    print(f"Process Created")
+                    self.set_breakpoint(0x401000)
+                    continue_status = DBG_CONTINUE
+
+                elif code == EXCEPTION_DEBUG_EVENT:
+                    exception_code = cast(
+                        debug_event.u,
+                        pointer(DWORD)
+                    )[0]
+
+                if exception_code == EXCEPTION_BREAKPOINT:
+                    print("Breakpoint hit")
+                    continue_status = DBG_CONTINUE
+
+                else:
+                    continue_status = DBG_EXCEPTION_NOT_HANDLED
+
+            elif code == EXIT_PROCESS_DEBUG_EVENT:
+                print("Process Exited")
+                break
+
+            else:
+                continue_status = DBG_CONTINUE
+
+            kernel32.ContinueDebugEvent(
+                debug_event.dwProcessId,
+                debug_event.dwThreadId,
+                continue_status
+            )
+
+if __name__ == "__main__":
+    dbg = Debugger()
+
+    if dbg.load("C:\\Windows\\SysWow64\\notepad.exe"):
+        dbg.run
+
